191376b90b7ab4a1571db4b6f3c8ceea538957b5
Cao Tamingo <tamingocao@outlook.com>
2017-03-16 20:00:27 -0400

    no message

--- ME555_Final/Assets/Shooting.cs
+++ ME555_Final/Assets/Shooting.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour {
     public GameObject bullet;
+    public GameObject player;
+    private Animator ani;
 	// Use this for initialization
 	void Start () {
-		
+        ani = player.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float lefttrigger = Input.GetAxis("Left_Trigger");
+        if (lefttrigger!=0)
+        {
+            ani.enabled = true;
+        }
+        else
+        {
+            ani.enabled = false;
+        }
 	}
 }
 
